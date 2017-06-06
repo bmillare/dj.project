aeca24750be872e165ba3334914ed6ed7269537c
Brent Millare <brent.millare@gmail.com>
2017-06-06 01:03:14 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.bmillare/dj.project "0.3.3"
+(defproject org.bmillare/dj.project "0.3.4"
   :description "dj dynamic utilities, load jars, load native jars, reload jars, and load projects via project.clj files on the fly"
   :url "https://github.com/bmillare/dj.project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.bmillare/dj.core "0.2.0"]
-                 [com.cemerick/pomegranate "0.3.0"]])
+                 [com.cemerick/pomegranate "0.3.1"]])
