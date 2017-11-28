f1713e53c50f9a172a7b82093eaa912acb594892
Brent Millare <brent.millare@gmail.com>
2017-11-28 18:08:01 -0500

    pomegranate version bump and notice for native class path modification for java 9+

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.bmillare/dj.project "0.3.5"
+(defproject org.bmillare/dj.project "1.0.0"
   :description "dj dynamic utilities, load jars, load native jars, reload jars, and load projects via project.clj files on the fly"
   :url "https://github.com/bmillare/dj.project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.bmillare/dj.core "0.2.0"]
-                 [com.cemerick/pomegranate "0.3.1"]]
+                 [com.cemerick/pomegranate "1.0.0"]]
   :exclusions [org.clojure/clojure])
--- src/dj/classloader.clj
+++ src/dj/classloader.clj
@@ -4,8 +4,7 @@
             [dj.repl]
 	    [cemerick.pomegranate :as pom]
 	    [dj.io]
-            [clojure.string])
-  (:refer-clojure :exclude (add-classpath)))
+            [clojure.string]))
 
 (defn reload-class-file
   "Reload a .class file during runtime, this allows you to recompile
@@ -19,7 +18,120 @@
 		  (dj.io/to-byte-array f)
 		  nil)))
 
-(defn reset-native-paths! [native-paths]
+(defn reset-native-paths!
+  "won't work in Java 9+ because of the removal of setAccessible. See
+  this stackoverflow answer for a new implementation strategy.
+
+  https://stackoverflow.com/questions/42052856/java-9-classpath-and-library-path-extension
+
+  public class MiscTools
+{
+    private static class SpclClassLoader extends URLClassLoader
+    {
+        static
+        {
+            ClassLoader.registerAsParallelCapable();
+        }
+
+        private final Set<Path> userLibPaths = new CopyOnWriteArraySet<>();
+
+        private SpclClassLoader()
+        {
+            super(new URL[0]);
+        }
+
+        @Override
+        protected void addURL(URL url)
+        {
+            super.addURL(url);
+        }
+
+        protected void addLibPath(String newpath)
+        {
+            userLibPaths.add(Paths.get(newpath).toAbsolutePath());
+        }
+
+        @Override
+        protected String findLibrary(String libname)
+        {
+            String nativeName = System.mapLibraryName(libname);
+            return userLibPaths.stream().map(tpath -> tpath.resolve(nativeName)).filter(Files::exists).map(Path::toString).findFirst().orElse(super.findLibrary(libname));            }
+    }
+    private final static SpclClassLoader ucl = new SpclClassLoader();
+
+    /**
+     * Adds a jar file or directory to the classpath. From Utils4J.
+     *
+     * @param newpaths JAR filename(s) or directory(s) to add
+     * @return URLClassLoader after newpaths added if newpaths != null
+     */
+    public static ClassLoader addToClasspath(String... newpaths)
+    {
+        if (newpaths != null)
+            try
+            {
+                for (String newpath : newpaths)
+                    if (newpath != null && !newpath.trim().isEmpty())
+                        ucl.addURL(Paths.get(newpath.trim()).toUri().toURL());
+            }
+            catch (IllegalArgumentException | MalformedURLException e)
+            {
+                RuntimeException re = new RuntimeException(e);
+                re.setStackTrace(e.getStackTrace());
+                throw re;
+            }
+        return ucl;
+    }
+
+    /**
+     * Adds to library path in ClassLoader returned by addToClassPath
+     *
+     * @param newpaths Path(s) to directory(s) holding OS library files
+     */
+    public static void addToLibraryPath(String... newpaths)
+    {
+        for (String newpath : Objects.requireNonNull(newpaths))
+            ucl.addLibPath(newpath);
+    }
+  }
+
+  Also add this at the start
+  Thread.currentThread().setContextClassLoader(MiscTools.addToClasspath());
+
+  For extended classpaths
+  try
+{
+    Class.forName(classname, true, MiscTools.addToClasspath(cptoadd);
+}
+catch (ClassNotFoundException IllegalArgumentException | SecurityException e)
+{
+    classlogger.log(Level.WARNING, "Error loading ".concat(props.getProperty("Class")), e);
+}
+
+  How to override classloader identification checks
+private final static CopyOnWriteArraySet<Driver> loadedDrivers = new CopyOnWriteArraySet<>();
+
+private static Driver isLoaded(String drivername, String... classpath) throws ClassNotFoundException
+{
+    Driver tdriver = loadedDrivers.stream().filter(d -> d.getClass().getName().equals(drivername)).findFirst().orElseGet(() ->
+    {
+        try
+        {
+            Driver itdriver = (Driver) Class.forName(drivername, true, addToClasspath(classpath)).newInstance();
+            loadedDrivers.add(itdriver);
+            return itdriver;
+        }
+        catch (ClassNotFoundException | IllegalAccessException | InstantiationException e)
+        {
+            return null;
+        }
+    });
+    if (tdriver == null)
+        throw new java.lang.ClassNotFoundException(drivername + " not found.");
+    return tdriver;
+}
+  "
+  [native-paths]
   ;; Reset java.library.path by setting sys_paths variable in
   ;; java.lang.ClassLoader to NULL, depends on java implementation
   ;; knowledge
